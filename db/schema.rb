cc367f01520aa2ac5961b7831e7c8d8e7bfd68f4
zkim <zachary.kim@gmail.com>
2010-10-10 22:08:42 -0600

    Added migration that adds version numbers to 3rd party libs

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101010212830) do
+ActiveRecord::Schema.define(:version => 20101011035100) do
 
   create_table "comments", :force => true do |t|
     t.integer  "commentable_id",                 :default => 0
