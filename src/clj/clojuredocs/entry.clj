de9e970bc7c45b0a252dbd398d3332e7e1a1fd4d
Zachary Kim <zachary.kim@gmail.com>
2013-11-14 16:55:26 -0800

    Manually add special forms to autocomplete

--- src/clj/clojuredocs/entry.clj
+++ src/clj/clojuredocs/entry.clj
@@ -70,6 +70,41 @@
                                         :column :added :static :doc :line])))]
     (clucy/add search-index nm)))
 
+(def special-forms
+  [{:name 'def
+    :ns "clojure.core"
+    :doc "Creates and interns or locates a global var with the name of symbol and a namespace of the value of the current namespace (*ns*). See http://clojure.org/special_forms for more information."}
+   {:name 'if
+    :ns "clojure.core"
+    :doc "Evaluates test."}
+   {:name 'do
+    :ns "clojure.core"
+    :doc "Evaluates the expressions in order and returns the value of the last. If no expressions are supplied, returns nil. See http://clojure.org/special_forms for more information."}
+   {:name 'quote
+    :ns "clojure.core"
+    :doc "Yields the unevaluated form. See http://clojure.org/special_forms for more information."}
+   {:name 'var
+    :ns "clojure.core"
+    :doc "The symbol must resolve to a var, and the Var object itself (not its value) is returned. The reader macro #'x expands to (var x). See http://clojure.org/special_forms for more information."}
+   {:name 'recur
+    :ns "clojure.core"
+    :doc "Evaluates the exprs in order, then, in parallel, rebinds the bindings of the recursion point to the values of the exprs."}
+   {:name 'throw
+    :ns "clojure.core"
+    :doc "Evaluates the exprs in order, then, in parallel, rebinds the bindings of the recursion point to the values of the exprs. "}
+   {:name 'try
+    :ns "clojure.core"
+    :doc "The exprs are evaluated and, if no exceptions occur, the value of the last is returned. If an exception occurs and catch clauses are provided, each is examined in turn and the first for which the thrown exception is an instance of the named class is considered a matching catch clause. If there is a matching catch clause, its exprs are evaluated in a context in which name is bound to the thrown exception, and the value of the last is the return value of the function. If there is no matching catch clause, the exception propagates out of the function. Before returning, normally or abnormally, any finally exprs will be evaluated for their side effects."}
+   {:name '.
+    :ns "clojure.core"
+    :doc "The '.' special form is the basis for access to Java. It can be considered a member-access operator, and/or read as 'in the scope of'."}
+   {:name 'set!
+    :ns "clojure.core"
+    :doc "Assignment special form. When the first operand is a field member access form, the assignment is to the corresponding field. If it is an instance field, the instance expr will be evaluated, then the expr. In all cases the value of expr is returned. Note - you cannot assign to function params or local bindings. Only Java fields, Vars, Refs and Agents are mutable in Clojure."}])
+
+(doseq [m special-forms]
+  (clucy/add search-index m))
+
 ;; https://gist.github.com/ck/960716
 (defn- compute-next-row
   "computes the next row using the prev-row current-element and the other seq"
@@ -102,7 +137,7 @@ Still maintains the O(n*m) guarantee.
 
 (defn lucene-escape [s]
   (-> s
-      (str/replace #"[\+\-\!\(\)\{\}\[\]\^\"\~\*\?\:\\]" "\\\\$0")))
+      (str/replace #"[\+\-\!\(\)\{\}\[\]\^\"\~\*\?\:\\\/]" "\\\\$0")))
 
 (defn search [query]
   (when-not (empty? query)
