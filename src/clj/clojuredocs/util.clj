954ca7a505fd6f92606fa6731edb595c900f612a
Zachary Kim <zachary.kim@gmail.com>
2013-12-12 01:17:31 -0800

    Add date info to examples

--- src/clj/clojuredocs/util.clj
+++ src/clj/clojuredocs/util.clj
@@ -34,3 +34,41 @@
      (org.bson.types.ObjectId.))
   ([id-or-str]
      (org.bson.types.ObjectId/massageToObjectId id-or-str)))
+
+(defn now [] (System/currentTimeMillis))
+
+(defn timeago [millis]
+  (when millis
+    (let [millis (if (number? millis)
+                   millis
+                   (.getTime millis))
+          ms (- (now) millis)
+          s (/ ms 1000)
+          m (/ s 60)
+          h (/ m 60)
+          d (/ h 24)
+          y (/ d 365.0)]
+      (cond
+        (< s 60) "less than a minute"
+        (< m 2) "1 minute"
+        (< h 1) (str (int m) " minutes")
+        (< h 2) "1 hour"
+        (< d 1) (str (int h) " hours")
+        (< d 2) "1 day"
+        (< y 1) (str (int d) " days")
+        :else (str (format "%.1f" y) " years")))))
+
+(defn munge-name [s]
+  (-> s
+      str
+      (str/replace #"\." "_dot")
+      (str/replace #"\/" "_div")))
+
+(defn unmunge-name [s]
+  (-> s
+      (str/replace #"_dot" ".")
+      (str/replace #"_div" "/")
+      (str/replace #"_qm" "?")))
+
+(defn $var-link [ns name text]
+  [:a {:href (str "/" ns "/" (munge-name name))} text])
