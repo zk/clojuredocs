007959838bad68d429f460a15cf3d908b7ad0724
Zachary Kim <zachary.kim@gmail.com>
2014-07-06 15:33:04 -0700

    Add footer

--- src/clj/clojuredocs/site/common.clj
+++ src/clj/clojuredocs/site/common.clj
@@ -52,13 +52,42 @@
     [:link {:rel :stylesheet :href "//fonts.googleapis.com/css?family=Open+Sans:400" :type "text/css"}]
     [:script "window.PAGE_DATA=" (util/to-json (pr-str page-data)) ";"]]
    [:body
-    (when body-class
-      {:class body-class})
-    ($navbar opts)
-    [:div.container
-     [:div.row
-      [:div.col-md-10.col-md-offset-1
-       content]]]
+    [:div.sticky-wrapper
+     (when body-class
+       {:class body-class})
+     ($navbar opts)
+     [:div.container
+      [:div.row
+       [:div.col-md-10.col-md-offset-1
+        content]]]
+     [:div.sticky-push]]
+    [:footer
+     [:div.divider
+      "⤜ ❦ ⤛"]
+     [:div.ctas
+      "Brought to you by "
+      [:a {:href "https://twitter.com/heyzk"} "@heyzk"]
+      ". "
+      "&nbsp; / "
+      [:iframe {:src "http://ghbtns.com/github-btn.html?user=zk&repo=clojuredocs&type=watch&count=true"
+                :allowtransparency "true"
+                :frameborder "0"
+                :scrolling "0"
+                :width "80"
+                :height "20"}]
+      [:iframe {:src "http://ghbtns.com/github-btn.html?user=zk&repo=clojuredocs&type=fork&count=true"
+                :allowtransparency "true"
+                :frameborder "0"
+                :scrolling "0"
+                :width "80"
+                :height "20"}]
+      [:a.twitter-share-button {:href "https://twitter.com/share"
+                                :data-url "https://www.clojuredocs.org"
+                                :data-text "Community-powered docs and examples for #Clojure"
+                                :data-via "heyzk"}
+       "Tweet"]
+      [:script
+       "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');"]]]
     (when (env/bool :cljs-dev)
       [:script {:src "http://fb.me/react-0.9.0.js"}])
     (when (env/bool :cljs-dev)
