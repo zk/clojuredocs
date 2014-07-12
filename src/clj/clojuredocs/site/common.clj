e292a5bd6c1ff9b64e84a34492c5a79c149c3380
Zachary Kim <zachary.kim@gmail.com>
2014-07-11 23:23:45 -0700

    Add morpheus to dev script includes

--- src/clj/clojuredocs/site/common.clj
+++ src/clj/clojuredocs/site/common.clj
@@ -93,6 +93,8 @@
       [:script
        "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');"]]]
     (when (env/bool :cljs-dev)
+      [:script {:src "/js/morpheus.min.js"}])
+    (when (env/bool :cljs-dev)
       [:script {:src "http://fb.me/react-0.9.0.js"}])
     (when (env/bool :cljs-dev)
       [:script {:type "text/javascript" :src "/cljs/goog/base.js"}])
