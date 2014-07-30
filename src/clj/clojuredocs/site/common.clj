131bea49c59b99242d777d76a2410c827c9ce022
Zachary Kim <zachary.kim@gmail.com>
2014-07-29 21:29:30 -0700

    Add fullscreen mode on apple devices

--- src/clj/clojuredocs/site/common.clj
+++ src/clj/clojuredocs/site/common.clj
@@ -107,6 +107,7 @@
   [:html5
    [:head
     [:meta {:name "viewport" :content "width=device-width, maximum-scale=1.0"}]
+    [:meta {:name "apple-mobile-web-app-capable" :content "yes"}]
     [:title (or title "Community-Powered Clojure Documentation and Examples | ClojureDocs")]
     font-awesome-link
     bootstrap-link
@@ -166,7 +167,27 @@
     clojuredocs-script
     (when (env/bool :cljs-dev)
       [:script "goog.require(\"clojuredocs.main\");"])
-    ($ga-script-tag config/ga-tracking-id)]])
+    ($ga-script-tag config/ga-tracking-id)
+    [:script
+     "if((\"standalone\" in window.navigator) && window.navigator.standalone){
+var noddy, remotes = false;
+
+document.addEventListener('click', function(event) {
+
+noddy = event.target;
+
+while(noddy.nodeName !== \"A\" && noddy.nodeName !== \"HTML\") {
+noddy = noddy.parentNode;
+}
+
+if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
+{
+event.preventDefault();
+document.location.href = noddy.href;
+}
+
+},false);
+}"]]])
 
 (defn $avatar [{:keys [email login avatar-url] :as user}]
   [:a {:href (str "/u/" login)}
