1cc174be9903ebe58a6def65d0c54a6b18e2b284
Ilya Bernshteyn <ilya.bernshteyn@rate.com>
2019-07-13 15:02:40 -0500

    Updated to Clojure 1.10.1
    Now we have the new vars and namespaces added in Clojure 1.10.1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clojuredocs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojuredocs "0.2.0"
+  :description "The clojuredocs.org web app"
+  :url "http://clojuredocs.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :source-paths ["src/clj" "src/cljc"]
   :test-paths ["test/clj"]
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.9.946"]
                  [nsfw "0.11.72" :exclusions
                   [org.clojure/core.async
