1dc81ed78d23207455d88c98f8f61c0b8fa7c4ab
Ilya Bernshteyn <ilya.bernshteyn@rate.com>
2019-07-14 14:33:17 -0500

    Reverted the project version, desc, and updated mobile nav
    Restored the original project version and description and bumped
    the Clojure version for the mobile nav. Also updated the README
    about the mobile nav.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojuredocs "0.2.0"
-  :description "The clojuredocs.org web app"
-  :url "http://clojuredocs.org"
+(defproject clojuredocs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
